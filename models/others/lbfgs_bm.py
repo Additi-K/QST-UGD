7413a472cf5f2153bb01dcf53f2bb512adc767e7
Kuchibhotla Aditi <42117050+Additi-K@users.noreply.github.com>
2025-03-19 11:41:27 -0600

    Update lbfgs_bm.py

--- models/others/lbfgs_bm.py
+++ models/others/lbfgs_bm.py
@@ -56,76 +56,74 @@ class lbfgs_nn(nn.Module):
 
 class lbfgs():
   def __init__(self, na_state, generator, P_star, learning_rate=0.01):
+      self.generator = generator
+      self.P_star = P_star
+      self.criterion = MLE_loss
 
-    super().__init__
-    
-    self.generator = generator
-    self.P_star = P_star
-    
-    self.criterion = MLE_loss
-
-    self.optim = optim.LBFGS(self.generator.parameters(), lr=0.1, max_iter=1e3, tolerance_grad=1e-07, tolerance_change=1e-09, history_size=10, line_search_fn=None)
-    self.overhead_t = 0
-    self.epoch = 0
-    
-  def track_parameters(loss, fid, result_save):
-    """Callback to store parameter updates (excluding computation time)."""
-
-    start_overhead = perf_counter()  # Start timing overhead
-    self.generator.eval()
-    with torch.no_grad():
-      rho = self.generator.rho
-      rho /= torch.trace(rho)
-      penalty = 0.5*2*torch.sum(self.P_star)*torch.norm(self.generator.params,  p=2)**2
-  
-      Fq = fid.Fidelity(rho)
-  
-      # result_save['time'].append(time_all)
-      result_save['epoch'].append(self.epoch)
-      result_save['Fq'].append(Fq)
-      result_save['loss'].append(loss.item()-penalty)
-      self.epoch += 1
-      # print("LBFGS_BM loss {:.10f} | Fq {:.8f} | time {:.5f}".format(loss-penalty, Fq, time_all)
-                
-    self.overhead_t = perf_counter() - start_overhead
-    
-  def train(self, epochs, fid, result_save):
-    """Net training"""
-    # self.sche = optim.lr_scheduler.StepLR(self.optim, step_size=1500, gamma=0.2)
-
-    pbar = tqdm(range(1), mininterval=0.01)
-    epoch = 0
-    time_all = 0
-    for i in pbar:
-        epoch += 1
-        time_b = perf_counter()
-
-        self.generator.train()
-
-        def closure():
-            self.optim.zero_grad()
-            data = self.P_star
-            P_out = self.generator()
-            loss = self.criterion(P_out, data)
-            loss += 0.5*2*torch.sum(self.P_star)*torch.norm(self.generator.params,  p=2)**2
-            assert torch.isnan(loss) == 0, print('loss is nan', loss)
-            loss.backward()
-            track_parameters(loss, fid, result_save)
-            raw_t = perf_counter()
-            t_all += raw_t - time_b - self.overhead_t
-            result_save['time'].append(t_all)
-
-            self.generator.train()
-            
-            return loss
-
-        self.optim.step(closure)
+      self.optim = optim.LBFGS(self.generator.parameters(), lr=0.1, max_iter=1000, 
+                               tolerance_grad=1e-07, tolerance_change=1e-09, 
+                               history_size=10, line_search_fn=None)
       
-    # Print tracked updates with timestamps (excluding extra computation time)
-    for i, (f, l, t) in enumerate(zip(result_save['Fq'], result_save['loss'], result_save['time'])):
-      print("LBFGS_BM loss {:.10f} | Fq {:.8f} | time {:.5f}".format(l, f, t))
+      self.overhead_t = 0
+      self.epoch = 0
+      self.time_all = 0 
 
-    pbar.close()
+  def track_parameters(self, loss, fid, result_save):
+      """Callback to store parameter updates (excluding computation time)."""
+
+      start_overhead = perf_counter()  # Start timing overhead
+      self.generator.eval()
+
+      with torch.no_grad():
+          rho = self.generator.rho
+          rho /= torch.trace(rho)
+          penalty = 0.5 * 2 * torch.sum(self.P_star) * torch.norm(self.generator.params, p=2) ** 2
+
+          Fq = fid.Fidelity(rho)
+
+          result_save['epoch'].append(self.epoch)
+          result_save['Fq'].append(Fq)
+          result_save['loss'].append(loss.item() - penalty)
+          self.epoch += 1
+
+      self.overhead_t = perf_counter() - start_overhead  # ✅ Correct overhead timing
+
+  def train(self, epochs, fid, result_save):
+      """Net training."""
+      pbar = tqdm(range(1), mininterval=0.01)
+      epoch = 0
+
+      for _ in pbar:
+          epoch += 1
+          
+
+          self.generator.train()
+
+          def closure():
+              self.generator.train()
+              time_b = perf_counter()
+              self.optim.zero_grad()
+              P_out = self.generator()
+              loss = self.criterion(P_out, self.P_star)
+              loss += 0.5 * 2 * torch.sum(self.P_star) * torch.norm(self.generator.params, p=2) ** 2
+              
+              assert not torch.isnan(loss), "Loss is NaN" 
+              loss.backward()
+              self.track_parameters(loss, fid, result_save)
+              # Update tracking (exclude overhead from time_all)
+              raw_t = perf_counter()
+              self.time_all += raw_t - time_b - self.overhead_t
+              result_save['time'].append(self.time_all)
+
+              return loss
+
+          self.optim.step(closure)
+
+      # Print tracked updates
+      for i, (f, l, t) in enumerate(zip(result_save['Fq'], result_save['loss'], result_save['time'])):
+          print("LBFGS_BM loss {:.10f} | Fq {:.8f} | time {:.5f}".format(l, f, t))
+
+      pbar.close()
 
   # def train(self, epochs, fid, result_save):
   #   """Net training"""
